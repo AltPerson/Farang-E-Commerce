2d53f612b89d2a08369a500302b9686468a52aef
AltPerson <nothinghardforyou@gmail.com>
2023-08-22 11:02:13 +0300

    page404 tranlation

---
+++ src/Data/translation/page404.ts
@@ -0,0 +1,12 @@
+export const page404 = {
+  en: {
+    title: 'Page not found',
+    back: 'Return to',
+    link: ' the main',
+  },
+  ua: {
+    title: 'Сторінку не знайдено',
+    back: 'Повернутися на',
+    link: ' головну',
+  },
+};
